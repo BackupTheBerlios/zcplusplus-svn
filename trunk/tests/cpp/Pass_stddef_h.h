cfde9b969a3c23c6dacd523481d7cd569aa1f57a
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2009-04-17 04:24:47 +0000

    initial script for maintaining preprocessor test cases.  Regenerate stddef.h family of test cases.

--- trunk/tests/cpp/Pass_stddef_h.h
+++ trunk/tests/cpp/Pass_stddef_h.h
@@ -1,15 +1,17 @@
+// Pass_stddef_h.h
 // (C)2009 Kenneth Boyd, license: MIT.txt
 
 #include <stddef.h>
 
 /* check that the required macros exist */
 /* XXX offsetof is not implemented yet XXX */
-#ifndef NULL
-#error NULL not defined
-#endif
 
 // all null constants are integer constant expressions evaluating to zero
-#if NULL
-#error NULL is true
+#ifdef NULL
+	#if NULL
+	#error NULL is true
+	#endif
 #else
+#error NULL is undefined
 #endif
+
