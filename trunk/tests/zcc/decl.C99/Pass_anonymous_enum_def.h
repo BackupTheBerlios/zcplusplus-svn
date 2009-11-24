152b49930626b56e8d73e76683da3bc49329bfcb
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2009-11-24 07:16:13 +0000

    more test cases for code coverage

---
+++ trunk/tests/zcc/decl.C99/Pass_anonymous_enum_def.h
@@ -0,0 +1,8 @@
+// decl.C99\Pass_anonymous_enum_def.h
+// using singly defined enum
+// (C)2009 Kenneth Boyd, license: MIT.txt
+
+enum {
+	x_factor = 1
+}
+
---
+++ trunk/tests/zcc/decl.C99/Pass_enum_def.h
@@ -0,0 +1,8 @@
+// decl.C99\Pass_enum_def.h
+// using singly defined enum
+// (C)2009 Kenneth Boyd, license: MIT.txt
+
+enum good_test {
+	x_factor = 1
+}
+
