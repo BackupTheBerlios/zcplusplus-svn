e46d01ab1011db36406b2c95f5aa3d1bfbe9c45c
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2010-03-25 03:35:02 +0000

    static assertion tests for operator ~

--- trunk/tests/zcc/staticassert.C1X/Pass_autosuccess.h
+++ trunk/tests/zcc/staticassert.C1X/Pass_autosuccess.h
@@ -1,6 +1,8 @@
 // staticassert.C1X\Pass_autosuccess.h
 // (C)2009,2010 Kenneth Boyd, license: MIT.txt
 
+#include <limits.h>
+
 _Static_Assert(1,"automatic success has failed");
 
 // check unary +
@@ -13,4 +15,16 @@ _Static_Assert(-'A',"automatic success has failed");
 
 // check unary !
 _Static_Assert(!0,"automatic success has failed");
+
+// check unary ~ (unsigned only, signed is target-specific testing)
+_Static_Assert(~0U,"automatic success has failed");
+_Static_Assert(~0UL,"automatic success has failed");
+_Static_Assert(~0ULL,"automatic success has failed");
+_Static_Assert(~1U,"automatic success has failed");
+_Static_Assert(~1UL,"automatic success has failed");
+_Static_Assert(~1ULL,"automatic success has failed");
+_Static_Assert(!~UINT_MAX,"automatic success has failed");
+_Static_Assert(!~ULONG_MAX,"automatic success has failed");
+_Static_Assert(!~ULLONG_MAX,"automatic success has failed");
+
 
--- trunk/tests/zcc/staticassert.C1X/Pass_autosuccess.hpp
+++ trunk/tests/zcc/staticassert.C1X/Pass_autosuccess.hpp
@@ -1,6 +1,8 @@
 // staticassert.C1X\Pass_autosuccess.hpp
 // (C)2009,2010 Kenneth Boyd, license: MIT.txt
 
+#include <limits.h>
+
 static_assert(1,"automatic success has failed");
 
 // check unary +
@@ -13,4 +15,16 @@ static_assert(-'A',"automatic success has failed");
 
 // check unary !
 static_assert(!0,"automatic success has failed");
+
+// check unary ~ (unsigned only, signed is target-specific testing)
+static_assert(~0U,"automatic success has failed");
+static_assert(~0UL,"automatic success has failed");
+static_assert(~0ULL,"automatic success has failed");
+static_assert(~1U,"automatic success has failed");
+static_assert(~1UL,"automatic success has failed");
+static_assert(~1ULL,"automatic success has failed");
+static_assert(!~UINT_MAX,"automatic success has failed");
+static_assert(!~ULONG_MAX,"automatic success has failed");
+static_assert(!~ULLONG_MAX,"automatic success has failed");
+
 
