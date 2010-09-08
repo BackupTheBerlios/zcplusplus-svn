87fee3cb79d22223a51b022767dcc78aabe79e93
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2010-09-08 03:17:14 +0000

    C, C++: Verify that definitions that also declar an object are accepted

---
+++ trunk/tests/zcc/decl.C99/Pass_class_def_decl.hpp
@@ -0,0 +1,20 @@
+// decl.C99\Pass_class_def_decl.hpp
+// using singly defined struct
+// (C)2010 Kenneth Boyd, license: MIT.txt
+
+class good_test {
+	int x_factor;
+} y;
+
+// exercise some declarations
+class good_test x1;
+const class good_test c1;
+class good_test const c2;
+volatile class good_test v1;
+class good_test volatile v2;
+const volatile class good_test cv1;
+volatile const class good_test cv2;
+const class good_test volatile cv3;
+volatile class good_test const cv4;
+class good_test const volatile cv5;
+class good_test volatile const cv6;
---
+++ trunk/tests/zcc/decl.C99/Pass_struct_def_decl.h
@@ -0,0 +1,20 @@
+// decl.C99\Pass_struct_def_decl.h
+// using singly defined struct
+// (C)2010 Kenneth Boyd, license: MIT.txt
+
+struct good_test {
+	int x_factor;
+} y;
+
+// exercise some declarations
+struct good_test x1;
+const struct good_test c1;
+struct good_test const c2;
+volatile struct good_test v1;
+struct good_test volatile v2;
+const volatile struct good_test cv1;
+volatile const struct good_test cv2;
+const struct good_test volatile cv3;
+volatile struct good_test const cv4;
+struct good_test const volatile cv5;
+struct good_test volatile const cv6;
