ba4d7c7c648eb03794accf383bcee8e8e4306209
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2011-03-27 05:59:31 +0000

    test global-level parsing of thread_local for classes

--- trunk/tests/zcc/decl.C99/Pass_class_def_decl.hpp
+++ trunk/tests/zcc/decl.C99/Pass_class_def_decl.hpp
@@ -1,20 +1,119 @@
 // decl.C99\Pass_class_def_decl.hpp
-// using singly defined struct
-// (C)2010 Kenneth Boyd, license: MIT.txt
+// (C)2009,2011 Kenneth Boyd, license: MIT.txt
+// using singly defined class
 
 class good_test {
-	int x_factor;
+	int x_factor
 } y;
 
-// exercise some declarations
-class good_test x1;
-const class good_test c1;
-class good_test const c2;
-volatile class good_test v1;
-class good_test volatile v2;
-const volatile class good_test cv1;
-volatile const class good_test cv2;
-const class good_test volatile cv3;
-volatile class good_test const cv4;
-class good_test const volatile cv5;
-class good_test volatile const cv6;
+// ringing the changes on extern
+extern class good_test x1;
+extern const class good_test x2;
+extern volatile class good_test x3;
+extern const volatile class good_test x4;
+extern volatile const class good_test x5;
+
+// ringing the changes on static
+// (don't test static const -- no chance to initialize before use)
+static class good_test x6;
+static volatile class good_test x7;
+static const volatile class good_test x8;
+static volatile const class good_test x9;
+
+// extern/static not in first postion is deprecated, but legal
+const extern class good_test x10;
+volatile extern class good_test x11;
+const extern volatile class good_test x12;
+const volatile extern class good_test x13;
+volatile extern const class good_test x14;
+volatile const extern class good_test x15;
+volatile static class good_test x16;
+const static volatile class good_test x17;
+const volatile static class good_test x18;
+volatile static const class good_test x19;
+volatile const static class good_test x20;
+
+// ringing the changes on thread_local extern
+extern thread_local class good_test x21;
+extern thread_local const class good_test x22;
+extern thread_local volatile class good_test x23;
+extern thread_local const volatile class good_test x24;
+extern thread_local volatile const class good_test x25;
+thread_local extern class good_test x26;
+thread_local extern const class good_test x27;
+thread_local extern volatile class good_test x28;
+thread_local extern const volatile class good_test x29;
+thread_local extern volatile const class good_test x30;
+
+// ringing the changes on thread_local static
+static thread_local class good_test x31;
+static thread_local const class good_test x32;
+static thread_local volatile class good_test x33;
+static thread_local const volatile class good_test x34;
+static thread_local volatile const class good_test x35;
+thread_local static class good_test x36;
+thread_local static const class good_test x37;
+thread_local static volatile class good_test x38;
+thread_local static const volatile class good_test x39;
+thread_local static volatile const class good_test x40;
+
+// thread_local extern not in first two postions is deprecated, but legal
+extern const thread_local class good_test x41;
+const extern thread_local class good_test x42;
+extern volatile thread_local class good_test x43;
+volatile extern thread_local class good_test x44;
+extern const thread_local volatile class good_test x45;
+extern const volatile thread_local class good_test x46;
+const extern thread_local volatile class good_test x47;
+const extern volatile thread_local class good_test x48;
+const volatile extern thread_local class good_test x49;
+extern volatile thread_local const class good_test x50;
+extern volatile const thread_local class good_test x51;
+volatile extern thread_local const class good_test x52;
+volatile extern const thread_local class good_test x53;
+volatile const extern thread_local class good_test x54;
+thread_local const extern class good_test x55;
+const thread_local extern class good_test x56;
+thread_local volatile extern class good_test x57;
+volatile thread_local extern class good_test x58;
+thread_local const extern volatile class good_test x59;
+thread_local const volatile extern class good_test x60;
+const thread_local extern volatile class good_test x61;
+const thread_local volatile extern class good_test x62;
+const volatile thread_local extern class good_test x63;
+thread_local volatile extern const class good_test x64;
+thread_local volatile const extern class good_test x65;
+volatile thread_local extern const class good_test x66;
+volatile thread_local const extern class good_test x67;
+volatile const thread_local extern class good_test x68;
+
+// thread_local static not in first two postions is deprecated, but legal
+static const thread_local class good_test x69;
+const static thread_local class good_test x70;
+static volatile thread_local class good_test x71;
+volatile static thread_local class good_test x72;
+static const thread_local volatile class good_test x73;
+static const volatile thread_local class good_test x74;
+const static thread_local volatile class good_test x75;
+const static volatile thread_local class good_test x76;
+const volatile static thread_local class good_test x77;
+static volatile thread_local const class good_test x78;
+static volatile const thread_local class good_test x79;
+volatile static thread_local const class good_test x80;
+volatile static const thread_local class good_test x81;
+volatile const static thread_local class good_test x82;
+thread_local const static class good_test x83;
+const thread_local static class good_test x84;
+thread_local volatile static class good_test x85;
+volatile thread_local static class good_test x86;
+thread_local const static volatile class good_test x87;
+thread_local const volatile static class good_test x88;
+const thread_local static volatile class good_test x89;
+const thread_local volatile static class good_test x90;
+const volatile thread_local static class good_test x91;
+thread_local volatile static const class good_test x92;
+thread_local volatile const static class good_test x93;
+volatile thread_local static const class good_test x94;
+volatile thread_local const static class good_test x95;
+volatile const thread_local static class good_test x96;
+
