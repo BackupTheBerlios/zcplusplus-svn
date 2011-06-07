f3055989f03f240e562b5922067f9cc12ab987eb
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2011-06-07 17:02:34 +0000

    adjust version to 0.0.9 now as planning to use ZCC version as a preprocessor guard clause in tests

--- trunk/_version.h
+++ trunk/_version.h
@@ -3,9 +3,9 @@
 /* (C)2010,2011 Kenneth Boyd, license: MIT.txt */
 
 #ifndef ZCC_VERSION
-#define ZCC_VERSION "0.0.8"
+#define ZCC_VERSION "0.0.9"
 #define ZCC_VERSION_MAJOR 0
 #define ZCC_VERSION_MINOR 0
-#define ZCC_VERSION_PATCH 8
+#define ZCC_VERSION_PATCH 9
 #endif
 
