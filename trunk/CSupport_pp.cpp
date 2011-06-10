24a2cbdd5cd8d3781c417866ac495d15f06030fe
zaimoni <zaimoni@b4372a03-5d62-0410-814a-8f46757a0b64>
2011-06-10 17:53:33 +0000

    C keyword is _Thread_local, not _Thread_Local #4/final

--- trunk/CSupport_pp.cpp
+++ trunk/CSupport_pp.cpp
@@ -408,7 +408,7 @@ BOOST_STATIC_ASSERT(NONATOMIC_PREPROC_OP_LB<C_PREPROC_OP_STRICT_UB);
 static const POD_pair<const char*,size_t> valid_keyword[]
 	=	{	DICT_STRUCT("__asm"),		// reserved to the implementation, so OK to make a keyword for C only
 			DICT_STRUCT("_Static_Assert"),	// C1X keywords not in C++0X
-			DICT_STRUCT("_Thread_Local"),
+			DICT_STRUCT("_Thread_local"),
 			DICT_STRUCT("restrict"),	// C99 keywords not in C++98
 			DICT_STRUCT("_Bool"),
 			DICT_STRUCT("_Complex"),
